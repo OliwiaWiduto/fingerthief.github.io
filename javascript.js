000252fda7621999ec66e95422b0db1d5f8e14d3
OliwiaWiduto <oliwia.widuto@gmail.com>
2020-09-25 15:38:40 +0100

    aos and scroll fix

--- javascript.js
+++
@@ -1,9 +0,0 @@
-$('.navigation-change').click(function() {
-  
-    $('.site-nav').toggleClass('site-nav--open', 500);
-    $(this).toggleClass('open');
-    $('.nav-element').toggleClass('nav-reveal');
-    
-       
-    
-})
